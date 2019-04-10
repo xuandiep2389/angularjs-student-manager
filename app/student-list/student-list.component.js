a75dd20e469f0f8980a9da05cc85c9270eded474
vu linh <xuandiep2389@gmail.com>
2019-04-10 12:00:18 +0700

    sort student list

--- app/student-list/student-list.component.js
+++ app/student-list/student-list.component.js
@@ -4,8 +4,22 @@
 angular.module('studentList')
     .component('studentList',{
         templateUrl: 'student-list/student-list.template.html',
-        controller: ['$http', function StudentListController($http) {
+        controller: ['$scope','$http', function StudentListController($scope,$http) {
             var self = this;
+
+            $scope.sortType = 'name'; // set the default sort type
+
+            $scope.sortRevert = false;
+
+            $scope.sortDown = function(keyname) {
+                $scope.sortRevert = false;
+                $scope.sortType = keyname;
+            };
+
+            $scope.sortUp = function(keyname) {
+                $scope.sortRevert = true;
+                $scope.sortType = keyname;
+            };
 
             $http.get('students/students.json').then(function (response) {
                 self.students = response.data;

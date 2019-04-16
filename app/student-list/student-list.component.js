e3100962bd016c426cf5a657033993c0832b05b1
xuandiep2389 <xuandiep2389@gmail.com>
2019-04-16 15:29:16 +0700

    change table configuration function

--- app/student-list/student-list.component.js
+++ app/student-list/student-list.component.js
@@ -2,16 +2,16 @@
 
 // register `studentList` component
 angular.module('studentList')
-    .component('studentList',{
+    .component('studentList', {
         templateUrl: 'student-list/student-list.template.html',
-        controller: ['$scope','$http', function StudentListController($scope,$http) {
+        controller: ['$scope', '$http', function StudentListController($scope, $http) {
             var self = this;
 
             $scope.sortType = 'name'; // set the default sort type
 
             $scope.sortRevert = false;
 
-            $scope.sort = function(keyname) {
+            $scope.sort = function (keyname) {
                 $scope.sortRevert = !$scope.sortRevert;
                 $scope.sortType = keyname;
             };
@@ -32,66 +32,23 @@ angular.module('studentList')
             $scope.address = false;
             $scope.phoneNum = false;
 
-            // //toggle status of columns
-            // $scope.toggleName = function(){
-            //     if($scope.name === true)
-            //         $scope.name = false;
-            //     else if($scope.name === false)
-            //         $scope.name = true;
-            // };
-            //
-            // $scope.toggleAge = function(){
-            //     if($scope.age === true)
-            //         $scope.age = false;
-            //     else if($scope.age === false)
-            //         $scope.age = true;
-            // };
-            //
-            // $scope.toggleGender = function(){
-            //     if($scope.gender === true)
-            //         $scope.gender = false;
-            //     else if($scope.gender === false)
-            //         $scope.gender = true;
-            // };
-            //
-            // $scope.toggleAddress = function(){
-            //     if($scope.address === true)
-            //         $scope.address = false;
-            //     else if($scope.address === false)
-            //         $scope.address = true;
-            // };
-            //
-            // $scope.togglePhoneNum = function(){
-            //     if($scope.phoneNum === true)
-            //         $scope.phoneNum = false;
-            //     else if($scope.phoneNum === false)
-            //         $scope.phoneNum = true;
-            // };
+            $scope.checkAll = true;
 
-            // show all the columns of table
-            $scope.toggleCheck = function () {
-                if ($scope.phoneNum == true) {
-                    $scope.uncheckAll();
+            $scope.check = function () {
+                if ($scope.checkAll) {
+                    $scope.name = true;
+                    $scope.age = true;
+                    $scope.gender = true;
+                    $scope.address = true;
+                    $scope.phoneNum = true;
                 } else {
-                    $scope.checkAll();
+                    $scope.name = false;
+                    $scope.age = false;
+                    $scope.gender = false;
+                    $scope.address = false;
+                    $scope.phoneNum = false;
                 }
+            }
 
-            };
-
-            $scope.checkAll = function () {
-                $scope.name = true;
-                $scope.age = true;
-                $scope.gender = true;
-                $scope.address = true;
-                $scope.phoneNum = true;
-            };
-
-            $scope.uncheckAll = function () {
-                $scope.name = false;
-                $scope.age = false;
-                $scope.gender = false;
-                $scope.address = false;
-                $scope.phoneNum = false;
-            };
         }]
     });
